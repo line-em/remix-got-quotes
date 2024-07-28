d20838f46e554d6ca0285b0825ee6b52f6e949c7
Aline Emily <1231866@isep.ipp.pt>
2024-07-28 15:06:43 +0100

    render character map on the index page, with basic styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,12 @@
-import { json, Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import {
+	json,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData
+} from "@remix-run/react";
 import "./tailwind.css";
 import { getCharacters } from "./data";
 
@@ -8,6 +16,8 @@ export const loader = async () => {
 };
 
 export function Layout({ children }: { children: React.ReactNode }) {
+	const characters = useLoaderData<typeof loader>();
+
 	return (
 		<html lang="en">
 			<head>
@@ -17,7 +27,22 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Links />
 			</head>
 			<body>
-				{children}
+				<main className="grid grid-rows-[auto_1fr] grid-cols-[minmax(150px,200px)_1fr] gap-4 w-full h-[100vh]">
+					<header className="col-span-2 p-2">Game of Thrones Quotes</header>
+					<nav className="flex flex-col gap-4 align-center justify-start">
+						<div className="join join-vertical">
+							{characters.map((character) => (
+								<button
+									className="btn join-item"
+									key={character}
+								>
+									{character}
+								</button>
+							))}
+						</div>
+					</nav>
+					{children}
+				</main>
 				<ScrollRestoration />
 				<Scripts />
 			</body>
